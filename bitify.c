5e7ed63137ddfe5f77a7425935123f192bccf957
gardir <gardiz@gmail.com>
2016-11-04 09:20:40 +0100

    Updated usage a little, and made code more maintainable

--- bitify.c
+++ bitify.c
@@ -1,19 +1,27 @@
 #include<stdio.h>
 #include<utf8proc.h>
 
+#define HEX 1
+#define SPACES 2
+#define HELP 4
+#define LAST_ARG 8
+
 int argparse(int len, char **a, int *mode)
 {
 	int i;
 	for (i=1; i<len; i++) {
 		if (a[i][0] == '-') {
-			if (a[i][1] == 'x') // 00000001 == HEX
-				(*mode) |= 1;
-			else if (a[i][1] == 's') // 00000010 == SPACES
-				(*mode) |= 2;
-			else if (a[i][1] == 'h') // 00000100 == HELP
-				(*mode) |= 4;
+			if (a[i][1] == 'x') { // 00000001 == HEX
+				(*mode) |= HEX;
+			} else if (a[i][1] == 's') { // 00000010 == SPACES
+				(*mode) |= SPACES;
+			} else if (a[i][1] == 'h') { // 00000100 == HELP
+				(*mode) |= HELP;
+			}
 		}
-		else return i;
+		else {
+			return i;
+		}
 	}
 	return i;
 }
@@ -24,27 +32,35 @@ void bitify(char *line, int mode)
 	while ((*line) != 0) {
 		char c = (*line);
 		char byte[8];
-		for (i=0; i<8; i++)
-			if ((c>>i) & 1)
+		for (i=0; i<8; i++) {
+			if ((c>>i) & 1) {
 				byte[7-i] = '1';
-			else
+			} else {
 				byte[7-i] = '0';
+			}
+		}
 		line++;
 		printf("%s", byte);
-		if (mode & 2)
+		if (mode & SPACES) {
 			printf(" ");
+		}
 	}
-	if (!(mode & 8)) {
+	
+	if (!(mode & LAST_ARG)) {
 		char c = ' ';
 		char byte[8];
-		for (i=0; i<8; i++)
-			if ((c>>i) & 1)
+		for (i=0; i<8; i++) {
+			if ((c>>i) & 1) {
 				byte[7-i] = '1';
-			else
+			} else {
 				byte[7-i] = '0';
+			}
+		}
+		
 		printf("%s", byte);
-		if (mode & 2)
+		if (mode & SPACES) {
 			printf(" ");
+		}
 	}
 }
 
@@ -55,17 +71,20 @@ void hexify(char *line, int mode)
 	while ((*line) != 0) {
 		char c = (*line);
 		printf("%c", hex[(c>>4)&mask]);
-		if (mode & 2)
+		if (mode & SPACES) {
 			printf("%c ", hex[c&mask]);
-		else
+		} else {
 			printf("%c", hex[c&mask]);
+		}
 		line++;
 	}
-	if (!(mode & 8)) {
+	
+	if (!(mode & LAST_ARG)) {
 		printf("%c", hex[(' '>>4)&mask]);
 		printf("%c", hex[' '&mask]);
-		if (mode & 2)
+		if (mode & SPACES) {
 			printf(" ");
+		}
 	}
 }
 
@@ -73,9 +92,9 @@ void usage()
 {
 	printf("bitify [options] The text to convert to binary\n");
 	printf("Options:\n");
-	printf("%3s - %s\n", "-x", "converts text to hex instead");
-	printf("%3s - %s\n", "-s", "adds space between each letter");
-	printf("%3s - %s\n", "-h", "shows this help menu");
+	printf("%3s ~ %s\n", "-x", "converts text to hex instead");
+	printf("%3s ~ %s\n", "-s", "adds space between each letter");
+	printf("%3s ~ %s\n", "-h", "shows this help menu");
 	printf("=== Examples ===\n");
 	char *text = "cool prog";
 	printf("bitify %s\n", text);
@@ -95,17 +114,19 @@ int main(int argc, char **argv)
 	}
 	int mode = 0;
 	int i = argparse(argc, argv, &mode);
-	if (mode & 4) {
+	if (mode & HELP) {
 		usage();
 		return 0;
 	}
 	for (; i<argc; i++) {
-		if (i+1 == argc)
-			mode |= 8; // 00001000 == LAST ARG
-		if (mode & 1)
+		if (i+1 == argc) {
+			mode |= LAST_ARG; // 00001000 == LAST ARG
+		}
+		if (mode & 1) {
 			hexify(argv[i], mode);
-		else
+		} else {
 			bitify(argv[i], mode);
+		}
 	}
 	printf("\n");
 	return 0;
